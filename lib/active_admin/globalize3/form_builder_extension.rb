196be32382d19607133904e5f8fb3fc7019fce75
Tema Bolshakov <artyom@rentini.com>
2012-12-27 18:35:29 +0400

    Show current locale first

--- lib/active_admin/globalize3/form_builder_extension.rb
+++ lib/active_admin/globalize3/form_builder_extension.rb
@@ -4,15 +4,16 @@ module ActiveAdmin
       extend ActiveSupport::Concern
 
       def translated_inputs(name = "Translations", &block)
+        available_locales = rray(I18n.default_locale).+(I18n.available_locales.sort).uniq
         form_buffers.last << template.content_tag(:div, class: "activeadmin-translations") do
           template.content_tag(:ul, class: "available-locales") do
-            I18n.available_locales.sort.map do |locale|
+            available_locales.map do |locale|
               template.content_tag(:li) do
                 template.content_tag(:a, I18n.t(:"active_admin.globalize3.language.#{locale}"), href: "#locale-#{locale}", :"data-locale" => locale)
               end
             end.join.html_safe
           end <<
-          I18n.available_locales.sort.map do |locale|
+          available_locales.map do |locale|
             translation = object.translations.find { |t| t.locale.to_s == locale.to_s }
             translation ||= object.translations.build(locale: locale)
             fields = proc do |form|
